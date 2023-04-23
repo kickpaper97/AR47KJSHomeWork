f3aee95563e33d74df40d703dce0f64dc171185f
김지섭 <kickpaper97@gmail.com>
2023-04-24 07:34:21 +0900

    김지섭 : 0421 숙제 Map 동적 할당 해제

---
+++ CPP/HomeWork0421/GameEngineMap.cpp
@@ -0,0 +1,194 @@
+﻿// GameEngineMap.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <map>
+#include <list>
+#include "GameEngineMap.h"
+
+template<typename DataType0, typename DataType1>
+void TestValue(DataType0 _Data0, DataType1 _Data1)
+{
+}
+
+class Item 
+{
+
+};
+
+class Inven 
+{
+public:
+    void AddItem(Item* Item);
+
+    //void AddWeapon(Item* Item);
+    //void AddArmor();
+    //void AddPotion();
+    //void AddQuestItem();
+};
+
+int main()
+{
+    GameEngineDebug::LeckCheck();
+
+    {
+        GameEngineMap Test;
+        Test.insert(GameEnginePair(10, rand()));
+        Test.insert(GameEnginePair(7, rand()));
+        Test.insert(GameEnginePair(15, rand()));
+        Test.insert(GameEnginePair(25, rand()));
+        Test.insert(GameEnginePair(17, rand()));
+        Test.insert(GameEnginePair(18, rand()));
+        Test.insert(GameEnginePair(2, rand()));
+        Test.insert(GameEnginePair(6, rand()));
+        Test.insert(GameEnginePair(5, rand()));
+        Test.insert(GameEnginePair(4, rand()));
+        Test.insert(GameEnginePair(9, rand()));
+        Test.insert(GameEnginePair(8, rand()));
+        Test.insert(GameEnginePair(1, rand()));
+
+        //{
+        //    GameEngineMap::iterator FindIter = Test.find(7);
+        //    GameEngineMap::iterator NextIter = Test.erase(FindIter);
+        //}
+        //{
+        //    GameEngineMap::iterator FindIter = Test.find(9);
+        //    GameEngineMap::iterator NextIter = Test.erase(FindIter);
+        //}
+
+        //{
+        //    GameEngineMap::iterator FindIter = Test.find(6);
+        //    GameEngineMap::iterator NextIter = Test.erase(FindIter);
+        //}
+
+        //{
+        //    GameEngineMap::iterator FindIter = Test.find(10);
+        //    GameEngineMap::iterator NextIter = Test.erase(FindIter);
+        //}
+
+
+        //{
+        //    GameEngineMap::iterator FindIter = Test.find(10);
+        //    GameEngineMap::iterator NextIter = Test.erase(FindIter);
+        //}
+
+
+        std::cout << "그냥 돌리기" <<  std::endl;
+        GameEngineMap::iterator StartIter = Test.begin();
+        GameEngineMap::iterator EndIter = Test.end();
+        for (; StartIter != EndIter; ++StartIter)
+        {
+            std::cout << StartIter->first << std::endl;
+        }
+
+        std::cout << "전위 순회" << std::endl;
+        Test.FirstOrder();
+        std::cout << "중위 순회" << std::endl;
+        Test.MidOrder();    
+        std::cout << "후위 순회" << std::endl;
+        
+
+        Test.Free();
+
+    }
+
+    return 1;
+
+    {
+        std::list<int> Value;
+        Value.push_back(20);
+        Value.push_back(30);
+
+        for (int& Test : Value)
+        {
+            // Test = 내부의 변수;
+            Test = 50;
+        }
+
+        //for (std::list<int>::iterator Iter : Value)
+        //{
+        //}
+
+        int a = 0;
+    }
+
+    {
+        std::pair<int, int> NewPair;
+        NewPair.first;
+        NewPair.second;
+
+
+        std::map<int, int> Test;
+
+        std::pair<std::map<int, int>::iterator, bool> Value0
+            = Test.insert(std::pair<int, int>(5, rand()));
+
+        // 키가 겹칠때는 무시하면서 리턴해주는게 정상적으로 들어간 노드를 리턴해준다.
+
+        std::pair<std::map<int, int>::iterator, bool> Value7
+            = Test.insert(std::pair<int, int>(5, rand()));
+
+        if (Value7.second == false)
+        {
+            int a = 0;
+            // 같은 키를 가진 녀석을 넣어줬다.
+        }
+
+        std::pair<std::map<int, int>::iterator, bool> Value1
+            = Test.insert(std::pair<int, int>(4, rand()));
+        std::pair<std::map<int, int>::iterator, bool> Value2
+            = Test.insert(std::pair<int, int>(7, rand()));
+        std::pair<std::map<int, int>::iterator, bool> Value3
+            = Test.insert(std::pair<int, int>(1, rand()));
+        std::pair<std::map<int, int>::iterator, bool> Value4
+            = Test.insert(std::pair<int, int>(2, rand()));
+        std::pair<std::map<int, int>::iterator, bool> Value5
+            = Test.insert(std::pair<int, int>(15, rand()));
+
+        std::map<int, int>::iterator FindIter = Test.find(1);
+        std::map<int, int>::iterator NextIter = Test.erase(FindIter);
+
+        int a = 0;
+
+        // 탐색을 용이하게 하기 위해서 사용한다.
+        // 사용성이 좋아요.
+
+        //for (const std::pair<int, int>& Value : Test)
+        //{
+
+        //}
+
+        // std::pair<int, int> Pair;
+        // int& Value = Pair;
+
+        std::map<int, int>::iterator StartIter = Test.begin();
+        std::map<int, int>::iterator EndIter = Test.end();
+
+        // for (; StartIter != EndIter; ++StartIter)
+        // {
+            // Key 
+            // StartIter->first = 20;
+
+            // std::cout << StartIter->first << std::endl;
+            // Value
+            //StartIter->second = 60;
+        // }
+
+        //for (const std::pair<int, int>& Value : Test)
+        //{
+        //    Value.first = 20;
+        //}
+    }
+
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
---
+++ CPP/HomeWork0421/GameEngineMap.h
@@ -0,0 +1,491 @@
+#pragma once
+#include <GameEngineBase/GameEngineDebug.h>
+
+typedef int KeyType;
+typedef int ValueType;
+
+class GameEnginePair 
+{
+public:
+	KeyType first;
+	ValueType second;
+
+public:
+	GameEnginePair()
+	{
+
+	}
+
+	GameEnginePair(const KeyType& _Key, const ValueType& _Value)
+		: first(_Key), second(_Value)
+	{
+
+	}
+};
+
+// 설명 :
+class GameEngineMap
+{
+public:
+
+	class MapNode 
+	{
+	public:
+		MapNode* Parent = nullptr;
+		MapNode* LeftChild = nullptr;
+		MapNode* RightChild = nullptr;
+		GameEnginePair Pair;
+
+		bool IsLeaf() 
+		{
+			return nullptr == LeftChild && nullptr == RightChild;
+		}
+
+		void ChangeChild(MapNode* _OldChild, MapNode* _NewChild)
+		{
+			if (_OldChild == LeftChild)
+			{
+				LeftChild = _NewChild;
+				if (nullptr != _NewChild)
+				{
+					_NewChild->Parent = this;
+				}
+				return;
+			}
+
+			if (_OldChild == RightChild)
+			{
+				RightChild = _NewChild;
+				if (nullptr != _NewChild)
+				{
+					_NewChild->Parent = this;
+				}
+				return;
+			}
+
+		}
+
+		MapNode* OverParentNode()
+		{
+			MapNode* ParentNode = Parent;
+
+			while (Pair.first > ParentNode->Pair.first)
+			{
+				ParentNode = ParentNode->Parent;
+
+				if (nullptr == ParentNode)
+				{
+					return nullptr;
+				}
+			}
+
+			return ParentNode;
+		}
+
+		MapNode* PrevNode()
+		{
+			return nullptr;
+		}
+
+		MapNode* NextNode()
+		{
+			if (nullptr != RightChild)
+			{
+				return RightChild->MinNode();
+			}
+
+			if (nullptr != Parent)
+			{
+				return OverParentNode();
+			}
+
+			return nullptr;
+		}
+
+		MapNode* MaxNode()
+		{
+			if (nullptr == RightChild)
+			{
+				return this;
+			}
+
+			return RightChild->MaxNode();
+		}
+
+		MapNode* MinNode() 
+		{
+			if (nullptr == LeftChild)
+			{
+				return this;
+			}
+
+			return LeftChild->MinNode();
+		}
+
+		bool insert(MapNode* _NewNode)
+		{
+			if (Pair.first > _NewNode->Pair.first)
+			{
+				if (nullptr != LeftChild)
+				{
+					return LeftChild->insert(_NewNode);
+				}
+
+				LeftChild = _NewNode;
+				_NewNode->Parent = this;
+				return true;
+			}
+			else if (Pair.first < _NewNode->Pair.first)
+			{
+				if (nullptr != RightChild)
+				{
+					return RightChild->insert(_NewNode);
+				}
+
+				RightChild = _NewNode;
+				_NewNode->Parent = this;
+				return true;
+			}
+
+			return false;
+		}
+
+		MapNode* find(KeyType _Key)
+		{
+			if (Pair.first > _Key)
+			{
+				if (nullptr != LeftChild)
+				{
+					return LeftChild->find(_Key);
+				}
+
+				return nullptr;
+			}
+			else if (Pair.first < _Key)
+			{
+				if (nullptr != RightChild)
+				{
+					return RightChild->find(_Key);
+				}
+
+				return nullptr;
+			}
+
+			return this;
+		}
+
+		void FirstOrder()
+		{
+			std::cout << Pair.first << std::endl;
+			if (nullptr != LeftChild)
+			{
+				LeftChild->FirstOrder();
+			}
+			if (nullptr != RightChild)
+			{
+				RightChild->FirstOrder();
+			}
+		}
+
+		void MidOrder()
+		{
+			if (nullptr != LeftChild)
+			{
+				LeftChild->MidOrder();
+			}
+			std::cout << Pair.first << std::endl;
+			if (nullptr != RightChild)
+			{
+				RightChild->MidOrder();
+			}
+
+		}
+
+		void LastOrder()
+		{
+			if (nullptr != LeftChild)
+			{
+				LeftChild->LastOrder();
+			}
+			if (nullptr != RightChild)
+			{
+				RightChild->LastOrder();
+			}
+
+			std::cout << Pair.first << std::endl;
+
+		}
+
+		void Free()
+		{
+
+			if (nullptr != LeftChild)
+			{
+				LeftChild->Free();
+				delete LeftChild;
+				LeftChild = nullptr;
+			}
+			if (nullptr != RightChild)
+			{
+				RightChild->Free();
+				delete RightChild;
+				RightChild = nullptr;
+			}
+
+
+		}
+
+		
+
+	};
+
+	class iterator
+	{
+		friend GameEngineMap;
+
+	public:
+		iterator()
+			: Node(nullptr)
+		{
+
+		}
+
+
+		iterator(class MapNode* _Node)
+			: Node(_Node)
+		{
+
+		}
+
+		GameEnginePair* operator->() 
+		{
+			return &Node->Pair;
+		}
+
+
+		bool operator!=(const iterator& _Other) const
+		{
+			return Node != _Other.Node;
+		}
+
+		bool operator==(const iterator& _Other) const 
+		{
+			return Node == _Other.Node;
+		}
+
+		iterator& operator++()
+		{
+			Node = Node->NextNode();
+
+			return *this;
+		}
+
+		iterator& operator--()
+		{
+			Node = Node->PrevNode();
+
+			return *this;
+			
+		}
+
+		
+
+		
+
+
+
+	private:
+		// 전방선언
+		class MapNode* Node = nullptr;
+	};
+
+
+
+	iterator begin()
+	{
+		if (nullptr == Root)
+		{
+			return end();
+		}
+
+		return iterator(Root->MinNode());
+	}
+
+	iterator end() 
+	{
+		return iterator();
+	}
+
+	iterator find(KeyType _Key)
+	{
+		if (nullptr == Root)
+		{
+			return end();
+		}
+
+		MapNode* FindNode = Root->find(_Key);
+
+		return iterator(FindNode);
+	}
+
+	iterator erase(const iterator& _EraseIter)
+	{
+		if (_EraseIter == end())
+		{
+			MsgBoxAssert("앤드를 삭제하려고 했습니다.");
+		}
+
+		MapNode* CurNode = _EraseIter.Node;
+		MapNode* ParentNode = CurNode->Parent;
+		MapNode* RightChild = CurNode->RightChild;
+		MapNode* LeftChild = CurNode->LeftChild;
+
+		MapNode* ChangeNode = nullptr;
+		MapNode* ChangeNodeParent = nullptr;
+		MapNode* NextNode = CurNode->NextNode();
+
+		if (true == CurNode->IsLeaf())
+		{
+			ParentNode->ChangeChild(CurNode, nullptr);
+			if (nullptr != CurNode)
+			{
+				delete CurNode;
+				CurNode = nullptr;
+			}
+			return NextNode;
+		}
+
+
+		// 교체될 노드는 절대로 양쪽을 모두 가진 노드일수 없다.
+		MapNode* ChangeChild = nullptr;
+		MapNode* ChangeParent = nullptr;
+
+		if (nullptr != LeftChild)
+		{
+			ChangeNode = LeftChild->MaxNode();
+			ChangeChild = ChangeNode->LeftChild;
+			ChangeParent = ChangeNode->Parent;
+		}
+		else if (nullptr != RightChild)
+		{
+			ChangeNode = RightChild->MinNode();
+			ChangeChild = ChangeNode->RightChild;
+			ChangeParent = ChangeNode->Parent;
+		}
+
+		if (nullptr == ChangeNode)
+		{
+			MsgBoxAssert("말도안돼");
+			return nullptr;
+		}
+
+		// 루트노드일 경우를 대비해서
+		// 체인지 노드의 뒷정리를 하는 기간
+		if (nullptr != ChangeParent)
+		{
+			ChangeParent->ChangeChild(ChangeNode, ChangeChild);
+		}
+
+
+		// 교체할 노드와 지워질 노드의 정보 교체를 한다.
+
+		if (nullptr != ParentNode)
+		{
+			ParentNode->ChangeChild(CurNode, ChangeNode);
+		}
+		else 
+		{
+			ChangeNode->Parent = nullptr;
+			Root = ChangeNode;
+			// RootNode 
+		}
+
+
+		ChangeNode->LeftChild = LeftChild;
+		if (nullptr != LeftChild)
+		{
+			LeftChild->Parent = ChangeNode;
+		}
+		ChangeNode->RightChild = RightChild;
+		if (nullptr != RightChild)
+		{
+			RightChild->Parent = ChangeNode;
+		}
+
+		if (nullptr != CurNode)
+		{
+			delete CurNode;
+			CurNode = nullptr;
+		}
+
+		return NextNode;
+	}
+
+	// ????
+	iterator rbegin()
+	{
+		if (nullptr == Root)
+		{
+			return end();
+		}
+
+		return iterator(Root->MinNode());
+	}
+	// ???
+	iterator rend()
+	{
+		if (nullptr == Root)
+		{
+			return end();
+		}
+
+		return iterator(Root->MinNode());
+	}
+
+
+	// 안들어가면 false리턴
+	bool insert(const GameEnginePair& _Pair)
+	{
+		if (nullptr == Root)
+		{
+			Root = new MapNode();
+			Root->Pair = _Pair;
+			return true;
+		}
+
+		MapNode* NewNode = new MapNode();
+		NewNode->Pair = _Pair;
+
+		if (false == Root->insert(NewNode))
+		{
+			delete NewNode;
+			NewNode = nullptr;
+		}
+
+		return true;
+	}
+
+	void FirstOrder() 
+	{
+		Root->FirstOrder();
+	}
+
+	void MidOrder()
+	{
+		Root->MidOrder();
+	}
+
+	void LastOrder()
+	{
+		Root->LastOrder();
+	}
+
+	void Free()
+	{
+		Root->Free();
+	}
+
+private:
+	MapNode* Root = nullptr;
+};
+
